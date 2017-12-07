d4240a09c0767ebdda6cee971f8e1c36b350f57c
kelvin27315 <seven_units@yahoo.co.jp>
2017-12-08 02:08:02 +0900

    fix

--- WordCloud.py
+++ WordCloud.py
@@ -13,13 +13,13 @@ if __name__ == "__main__":
 
 def Get_toots():
     #Mastodonから一日のtootsを取得する
-    temp = dt.date.today()
-    today = dt.datetime(temp.year, temp.month, temp.day - 2, 15, 0, 0, 0, tzinfo = tzutc())
+    temp = dt.date.today() - dt.timedelta(days=2)
+    today = dt.datetime(temp.year, temp.month, temp.day, 15, 0, 0, 0, tzinfo = tzutc())
 
     toots = mastodon.timeline(timeline = "local", limit = 40)
     while True:
         time = toots[-1]["created_at"]
-        #取得したget_toots全てのtootが0:00より前の場合終了
+        #取得したget_toots全てのtootが(JST)0:00より前の場合終了
         if time < today:
             break
         toots = toots + mastodon.timeline(timeline = "local", max_id = toots[-1]["id"] -1, limit = 40)
@@ -55,15 +55,16 @@ def Make_WordCloud(words):
     #Word Cloudを作成し、画像を保存する
     fpath = "NotoSansCJK-Regular"
     stop_words = ["てる", "さん", "こと", "する", "ある", "いる", "それ", "れる", "られ", "なっ", "そう", "なる", "よう",
-        "もう", "あれ", "ない", "いい", "思っ", "もの", "みたい", "感じ", "やっ", "どう", "あり"]
+        "もう", "あれ", "ない", "いい", "思っ", "もの", "みたい", "感じ", "やっ", "どう", "あり", "ちゃん", "あっ", "あと",
+        "とりあえず", "すぎる", "まあ", "ちょっと", "みんな"]
     wordcloud = WordCloud(font_path = fpath, width = 800, height = 600, stopwords=set(stop_words)).generate(words)
     wordcloud.to_file(filename = "wordcloud.png")
 
 def Toot():
     #日付と画像を投稿する
     media = [mastodon.media_post("wordcloud.png")]
-    today = dt.date.today()
-    post = str(today.month) + '月' + str(today.day - 1) + "日のトレンドです。 " + media[0]["text_url"]
+    today = dt.date.today() - dt.timedelta(days=1)
+    post = str(today.month) + '月' + str(today.day) + "日のトレンドです。 " + media[0]["text_url"]
     mastodon.status_post(post, media_ids = media)
 
 if __name__ == "__main__":
