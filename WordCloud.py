c314d997c7425512aa3647fa33fcb4cc5a30014d
kelvin27315 <seven_units@yahoo.co.jp>
2017-12-08 03:45:29 +0900

    update

--- WordCloud.py
+++ WordCloud.py
@@ -13,16 +13,19 @@ if __name__ == "__main__":
 
 def Get_toots():
     #Mastodonから一日のtootsを取得する
-    temp = dt.date.today() - dt.timedelta(days=1)
-    #JST
-    today = timezone("Asia/Tokyo").localize(dt.datetime(temp.year, temp.month, temp.day, 0, 0, 0, 0))
 
+    #JSTで昨日の0:00を指定
+    temp = dt.date.today() - dt.timedelta(days=1)
+    today = timezone("Asia/Tokyo").localize(dt.datetime(temp.year, temp.month, temp.day, 0, 0, 0, 0))
+    #tootの取得
     toots = mastodon.timeline(timeline = "local", limit = 40)
     while True:
+        #UTCからJSTに変更
         time = toots[-1]["created_at"].astimezone(timezone("Asia/Tokyo"))
-        #取得したget_toots全てのtootが(JST)0:00より前の場合終了
+        #取得したget_toots全てのtootが0:00より前の場合終了
         if time < today:
             break
+        #追加でtootの取得
         toots = toots + mastodon.timeline(timeline = "local", max_id = toots[-1]["id"] -1, limit = 40)
 
     f = open("toots_content.txt", 'w')
@@ -31,6 +34,7 @@ def Get_toots():
     f = open("toots_content.txt", 'a')
     #hiduke wo
     for toot in toots:
+        #HTMLタグとURLを取り除く
         text = re.sub(r"<[^>]*?>", '', toot["content"])
         text = re.sub(r"(https?|ftp)(:\/\/[-_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+\$,%#]+)",'', text)
         f.write(text)
@@ -45,6 +49,7 @@ def Wkati():
     f.close()
 
     words = ""
+    #分かち書きを行い単語の品詞が形容詞、動詞、名詞、副詞のみを取得する
     for word in m.parse(text).splitlines():
         if word != 'EOS':
             wtype = word.split('\t')[1].split(',')[0]
@@ -63,6 +68,7 @@ def Make_WordCloud(words):
 
 def Toot():
     #日付と画像を投稿する
+    #画像のURLが返ってくる
     media = [mastodon.media_post("wordcloud.png")]
     today = dt.date.today() - dt.timedelta(days=1)
     post = str(today.month) + '月' + str(today.day) + "日のトレンドです。 " + media[0]["text_url"]
