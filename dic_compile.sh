0c4b829b6637fe7643b19e0bb68cf91af33897c2
kelvin27315 <seven_units@yahoo.co.jp>
2019-12-19 22:29:28 +0900

    ディレクトリ名長すぎ

--- dic_compile.sh
+++ dic_compile.sh
@@ -2,8 +2,8 @@
 #Arch Linux
 CURRENT=$(cd $(dirname $0) && pwd)
 cd $CURRENT
-for dic in `ls dictionary | grep .csv`
+for dic in `ls dic | grep .csv`
 do
-    echo "/usr/lib/mecab/mecab-dict-index -d /usr/lib/mecab/dic/mecab-ipadic-neologd -u dictionary/compiled/$dic -f utf-8 -t utf-8 dictionary/$dic" | sed -r "s/\.csv/.dic/" | bash
+    echo "/usr/lib/mecab/mecab-dict-index -d /usr/lib/mecab/dic/mecab-ipadic-neologd -u dic/compiled/$dic -f utf-8 -t utf-8 dic/$dic" | sed -r "s/\.csv/.dic/" | bash
 done
-echo "userdic = $CURRENT/dictionary/compiled/gensokyo_town_terms.dic, $CURRENT/dictionary/compiled/touhou_character_names.dic, $CURRENT/dictionary/compiled/touhou_music_names.dic, $CURRENT/dictionary/compiled/touhou_spell_card_names.dic, $CURRENT/dictionary/compiled/touhou_terms.dic, $CURRENT/dictionary/compiled/touhou_work_names.dic, $CURRENT/dictionary/compiled/touhou_doujin_terms.dic" | sudo tee -a /usr/lib/mecab/dic/mecab-ipadic-neologd/dicrc
+echo "userdic = $CURRENT/dic/compiled/gensokyo_town_terms.dic, $CURRENT/dic/compiled/touhou_character_names.dic, $CURRENT/dic/compiled/touhou_music_names.dic, $CURRENT/dic/compiled/touhou_spell_card_names.dic, $CURRENT/dic/compiled/touhou_terms.dic, $CURRENT/dic/compiled/touhou_work_names.dic, $CURRENT/dic/compiled/touhou_doujin_terms.dic" | sudo tee -a /usr/lib/mecab/dic/mecab-ipadic-neologd/dicrc
