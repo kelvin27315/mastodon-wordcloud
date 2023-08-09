015bf4dcdfa268451cc59d04719b5695499bdffe
kelvin27315 <seven_units@yahoo.co.jp>
2023-08-10 02:55:06 +0900

    blackでformat

--- src/WordCloud.py
+++ src/WordCloud.py
@@ -11,11 +11,13 @@ from wordcloud import WordCloud
 PATH = path.dirname(path.abspath(__file__))
 if __name__ == "__main__":
     mastodon = Mastodon(
-            client_id = PATH + "/clientcred.secret",
-            access_token = PATH + "/usercred.secret",
-            api_base_url = "https://gensokyo.town")
+        client_id=PATH + "/clientcred.secret",
+        access_token=PATH + "/usercred.secret",
+        api_base_url="https://gensokyo.town",
+    )
 TODAY = dt.date.today()
 YESTERDAY = TODAY - dt.timedelta(days=1)
+
 
 def Extract_content(toots):
     """
@@ -23,52 +25,54 @@ def Extract_content(toots):
     CWが使用されている場合はspoiler_textを集める。
     また、使用するtootの数も数える。
     """
-    #1日の終わりの時刻(JST)
+    # 1日の終わりの時刻(JST)
     end = timezone("Asia/Tokyo").localize(dt.datetime(TODAY.year, TODAY.month, TODAY.day, 0, 0, 0, 0))
-    #1日の始まりの時刻(JST)
+    # 1日の始まりの時刻(JST)
     start = timezone("Asia/Tokyo").localize(dt.datetime(YESTERDAY.year, YESTERDAY.month, YESTERDAY.day, 0, 0, 0, 0))
     text = ""
     num = 0
     for toot in toots:
-        #時間内のtootのみcontentを追加する
+        # 時間内のtootのみcontentを追加する
         time = toot["created_at"].astimezone(timezone("Asia/Tokyo"))
         if start <= time and time < end:
-            #CWの呟きの場合隠されている方を追加せず表示されている方を追加する
+            # CWの呟きの場合隠されている方を追加せず表示されている方を追加する
             num += 1
             if toot["sensitive"] == True:
                 text = text + " " + toot["spoiler_text"]
             else:
                 text = text + " " + toot["content"]
-    #HTMLタグ, URL, LSEP,RSEP, 絵文字, HTML特殊文字を取り除く
+    # HTMLタグ, URL, LSEP,RSEP, 絵文字, HTML特殊文字を取り除く
     text = re.sub(r"<[^>]*?>", "", text)
     text = re.sub(r"(https?|ftp)(:\/\/[-_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+\$,%#]+)", "", text)
     text = re.sub(r"[  ]", "", text)
     text = re.sub(r"&[a-zA-Z0-9]+;", "", text)
-    return(text, num)
+    return (text, num)
+
 
 def Get_toots():
     """
     Mastodonから前日1日分のtootを取得し、呟き内容を保存する。
     また、前日1日分の取得したtootの数も返す
     """
-    #1日の始まりの時刻(JST)
+    # 1日の始まりの時刻(JST)
     start = timezone("Asia/Tokyo").localize(dt.datetime(YESTERDAY.year, YESTERDAY.month, YESTERDAY.day, 0, 0, 0, 0))
-    #tootの取得
+    # tootの取得
     toots = mastodon.timeline(timeline="local", limit=40)
     while True:
-        #UTCからJSTに変更
+        # UTCからJSTに変更
         time = toots[-1]["created_at"].astimezone(timezone("Asia/Tokyo"))
-        #取得したget_toots全てのtootが0:00より前の場合終了
+        # 取得したget_toots全てのtootが0:00より前の場合終了
         if time < start:
             break
-        #追加でtootの取得
-        toots = toots + mastodon.timeline(timeline = "local", max_id = toots[-1]["id"] - 1, limit = 40)
-    #取得したtootのリストからcontent(CWはspoiler_text)を抜き出す
+        # 追加でtootの取得
+        toots = toots + mastodon.timeline(timeline="local", max_id=toots[-1]["id"] - 1, limit=40)
+    # 取得したtootのリストからcontent(CWはspoiler_text)を抜き出す
     text, num = Extract_content(toots)
 
-    with open(PATH + "/toots_log/" + str(YESTERDAY) + ".txt", 'w') as f:
+    with open(PATH + "/toots_log/" + str(YESTERDAY) + ".txt", "w") as f:
         f.write(text)
-    return(num)
+    return num
+
 
 def Emoji_lanking():
     """
@@ -77,81 +81,135 @@ def Emoji_lanking():
     with open(PATH + "/toots_log/" + str(YESTERDAY) + ".txt", "r") as f:
         text = f.read()
 
-    #保存されたtootから絵文字だけ取り出してそれの出現回数のSeriesができる
+    # 保存されたtootから絵文字だけ取り出してそれの出現回数のSeriesができる
     emoji = pd.Series(re.findall(r":[a-zA-Z0-9_-]+:", text)).value_counts()
-    emoji = pd.DataFrame(emoji).reset_index().sort_values(by=[0,"index"], ascending=[False,True]).reset_index(drop=True)
+    emoji = (
+        pd.DataFrame(emoji).reset_index().sort_values(by=[0, "index"], ascending=[False, True]).reset_index(drop=True)
+    )
     temp_rank = 0
     temp_num = 0
-    toot = "{}月{}日に使用された絵文字の使用回数ランキングです。\n".format(YESTERDAY.month,YESTERDAY.day)
-    #ランキング作る
+    toot = "{}月{}日に使用された絵文字の使用回数ランキングです。\n".format(YESTERDAY.month, YESTERDAY.day)
+    # ランキング作る
     for i, em in emoji.iterrows():
         if em[0] == temp_num:
-            temp = "{}位: {} ({}回)\n".format(temp_rank,em["index"],em[0])
+            temp = "{}位: {} ({}回)\n".format(temp_rank, em["index"], em[0])
         else:
-            temp = "{}位: {} ({}回)\n".format(i+1,em["index"],em[0])
+            temp = "{}位: {} ({}回)\n".format(i + 1, em["index"], em[0])
             temp_num = em[0]
-            temp_rank = i+1
-        #500文字超えたら一度投稿して再度文を作り始める。
+            temp_rank = i + 1
+        # 500文字超えたら一度投稿して再度文を作り始める。
         if len(toot) + len(temp) >= 500:
-            mastodon.status_post(status = toot, visibility = "unlisted")
+            mastodon.status_post(status=toot, visibility="unlisted")
             toot = ""
         toot += temp
-    mastodon.status_post(status = toot, visibility = "unlisted")
+    mastodon.status_post(status=toot, visibility="unlisted")
+
 
 def Wkati():
     """
     取得したtootのcontent類に分かち書きを行う。
     必要な品詞だけ使用し、品詞よっては単語の原型を使用する。
     """
-    #MeCab(NEologd辞書使用)による分かち書き
+    # MeCab(NEologd辞書使用)による分かち書き
     m = MeCab.Tagger("-d /usr/lib/mecab/dic/mecab-ipadic-neologd")
     with open(PATH + "/toots_log/" + str(YESTERDAY) + ".txt", "r") as f:
         text = f.read()
-    #カスタム絵文字を取り除く
+    # カスタム絵文字を取り除く
     text = re.sub(r":[a-zA-Z0-9_-]+:", "", text)
 
     words = ""
-    #使用する品詞細分類1のリスト
+    # 使用する品詞細分類1のリスト
     norns = ["サ変接続", "ナイ形容詞語幹", "一般", "引用文字列", "形容動詞語幹", "固有名詞", "接続詞的", "接尾", "動詞非自立的", "特殊", "副詞可能"]
     adjectives = ["自立", "非自立"]
-    #分かち書きを行い単語の品詞が形容詞、動詞、名詞、副詞のみを取得する
+    # 分かち書きを行い単語の品詞が形容詞、動詞、名詞、副詞のみを取得する
     for word in m.parse(text).splitlines():
         if word != "EOS":
-            wtype = word.split('\t')[1].split(',')[0]   #品詞
-            wtype2 = word.split('\t')[1].split(',')[1]  #品詞細分類1
-            #名詞はそのまま,形容詞、動詞、副詞は原型を使用する
+            wtype = word.split("\t")[1].split(",")[0]  # 品詞
+            wtype2 = word.split("\t")[1].split(",")[1]  # 品詞細分類1
+            # 名詞はそのまま,形容詞、動詞、副詞は原型を使用する
             if wtype == "名詞" and wtype2 in norns:
-                words = words + " " + word.split('\t')[0]
+                words = words + " " + word.split("\t")[0]
             elif wtype == "形容詞" and wtype2 in adjectives:
-                words = words + " " + word.split('\t')[1].split(',')[6]
+                words = words + " " + word.split("\t")[1].split(",")[6]
             elif wtype == "動詞" and wtype2 == "自立":
-                words = words + " " + word.split('\t')[1].split(',')[6]
+                words = words + " " + word.split("\t")[1].split(",")[6]
             elif wtype == "副詞":
-                words = words + " " + word.split('\t')[1].split(',')[6]
-    return(words)
+                words = words + " " + word.split("\t")[1].split(",")[6]
+    return words
+
 
 def Make_WordCloud(words):
     """
     WordCloudモジュールを使用しワードクラウドの画像を作成する。
     """
     fpath = PATH + "/FJS-NewRodinProN-B.otf"
-    stop_words = ["てる", "さん", "こと", "する", "ある", "いる", "それ", "れる", "られ", "なっ", "そう", "なる", "よう",
-        "もう", "あれ", "ない", "いい", "思っ", "もの", "みたい", "感じ", "やっ", "どう", "あり", "ちゃん", "あっ", "あと",
-        "とりあえず", "すぎる", "まあ", "ちょっと", "みんな", "これ", "よく", "思う", "やる", "見る", "くる", "好き", "良い",
-        "いう", "言う", "出る", "ここ", "行く", "出来る", "できる", "られる", "わかる", "いく"]
+    stop_words = [
+        "てる",
+        "さん",
+        "こと",
+        "する",
+        "ある",
+        "いる",
+        "それ",
+        "れる",
+        "られ",
+        "なっ",
+        "そう",
+        "なる",
+        "よう",
+        "もう",
+        "あれ",
+        "ない",
+        "いい",
+        "思っ",
+        "もの",
+        "みたい",
+        "感じ",
+        "やっ",
+        "どう",
+        "あり",
+        "ちゃん",
+        "あっ",
+        "あと",
+        "とりあえず",
+        "すぎる",
+        "まあ",
+        "ちょっと",
+        "みんな",
+        "これ",
+        "よく",
+        "思う",
+        "やる",
+        "見る",
+        "くる",
+        "好き",
+        "良い",
+        "いう",
+        "言う",
+        "出る",
+        "ここ",
+        "行く",
+        "出来る",
+        "できる",
+        "られる",
+        "わかる",
+        "いく",
+    ]
     wordcloud = WordCloud(
-        font_path = fpath, width = 800, height = 600, stopwords = set(stop_words),
-        max_font_size = 180, collocations = False).generate(words)
-    wordcloud.to_file(filename = PATH + "/wordcloud.png")
+        font_path=fpath, width=800, height=600, stopwords=set(stop_words), max_font_size=180, collocations=False
+    ).generate(words)
+    wordcloud.to_file(filename=PATH + "/wordcloud.png")
+
 
 def Toot(num):
     """
     Mastodonに画像をアップロードし、文字と画像を投稿する
     """
-    #画像のURLが返ってくる
+    # 画像のURLが返ってくる
     media = [mastodon.media_post(PATH + "/wordcloud.png")]
     post = str(YESTERDAY.month) + "月" + str(YESTERDAY.day) + "日のトレンドです。（取得toot数: " + str(num) + "）"
-    mastodon.status_post(post, media_ids = media)
+    mastodon.status_post(post, media_ids=media)
+
 
 if __name__ == "__main__":
     num = Get_toots()
--- src/w2v/plot.py
+++ src/w2v/plot.py
@@ -11,11 +11,12 @@ def w2v_pca(vectors):
     pca = PCA(2)
     pca.fit(vectors)
     pca_vec = pca.transform(vectors)
-    return(pca_vec)
+    return pca_vec
+
 
 if __name__ == "__main__":
-    model = models.word2vec.Word2Vec.load(str(Path(__file__).parent.resolve()/"word2vec.gensim.model"))
-    with open(str(Path(__file__).parent.resolve()/".."/"dic"/"touhou_character_names.csv"),"r") as f:
+    model = models.word2vec.Word2Vec.load(str(Path(__file__).parent.resolve() / "word2vec.gensim.model"))
+    with open(str(Path(__file__).parent.resolve() / ".." / "dic" / "touhou_character_names.csv"), "r") as f:
         characters = f.read()
     characters = [ch.split(",")[0] for ch in characters.splitlines()]
     characters = [ch for ch in characters if ch in model.wv.vocab]
@@ -23,10 +24,7 @@ if __name__ == "__main__":
     vectors = w2v_pca(vectors)
 
     plt.figure(dpi=300)
-    plt.scatter(vectors[:,0], vectors[:,1], s=7)
-    for word,vec in zip(characters,vectors):
-        plt.annotate(word,(vec[0],vec[1]),size=4)
+    plt.scatter(vectors[:, 0], vectors[:, 1], s=7)
+    for word, vec in zip(characters, vectors):
+        plt.annotate(word, (vec[0], vec[1]), size=4)
     plt.savefig("w2v.png")
-
-
-
